b63c2978c4b807df6290f6226776266270c07f77
YXKJ019 <YXKJ019@172.20.16.46>
2018-12-13 09:37:26 +0800

    IU-2018.1.3 <YXKJ019@DESKTOP-4KA333D Create baseRefactoring.xml, ide.general.xml, Default.xml, project.default.xml, laf.xml, other.xml, Class.java, jdk.table.xml, debugger.renderers.xml, Default.xml, path.macros.xml, Solarized Dark.icls, find.xml, ui.lnf.xml, filetypes.xml, projectView.xml, editor.codeinsight.xml, colors.scheme.xml, Solarized Light.icls, databaseSettings.xml, databaseDrivers.xml, editor.xml, debugger.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,8 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * @Description:
+ * @Date: ${DATE} ${TIME}
+ */
+public class ${NAME} {
+}
